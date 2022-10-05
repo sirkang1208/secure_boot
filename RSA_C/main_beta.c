686b888c82315cd1bc0836bcd236ce56e51d4bc3
bpsswu <bpsswu@khu.ac.kr>
2022-10-05 16:12:03 +0900

    Update main_beta.c

--- RSA_C/main_beta.c
+++ RSA_C/main_beta.c
@@ -3,11 +3,11 @@
     This version is for Windows Visual Studio.
 */
 
-// #define _CRT_SECURE_NO_WARNINGS
-// #include <openssl/applink.c>
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <openssl/applink.c>
 #include <openssl/sha.h>
 #include <openssl/bn.h>
 #include <openssl/rsa.h>
@@ -24,10 +24,10 @@
     argv[2] == "./input/private_key_1.pem"
     argv[3] == "./input/public_key_2.pem"
 */
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     // Load 2nd bootloader's image
-    FILE *bl2_fp = fopen(argv[1], "rb");
+    FILE* bl2_fp = fopen(argv[1], "rb");
     if (bl2_fp == NULL)
     {
         printf("\n[ERR] failed to open 2nd Bootloader File\n");
@@ -43,26 +43,26 @@ int main(int argc, char *argv[])
     // memory allocation
     // bl2 : byte array which saves image of 2nd bootloader
     // hash_input : byte array which saves total image(public key + BL2)
-    unsigned char *bl2 = malloc(bl2_size);
-    unsigned char *hash_input = malloc(total_size);
+    unsigned char* bl2 = malloc(bl2_size);
+    unsigned char* hash_input = malloc(total_size);
 
     fread(bl2, 1, bl2_size, bl2_fp);
 
     // Load App Vendor's Public Key
-    FILE *pub2_fp = fopen(argv[3], "r");
-    if(pub2_fp == NULL)
+    FILE* pub2_fp = fopen(argv[3], "r");
+    if (pub2_fp == NULL)
     {
         printf("\n[ERR] failed to open Application Vendor's Public Key File\n");
         return -1;
     }
-    RSA *rsa_pub2 = PEM_read_RSA_PUBKEY(pub2_fp, NULL, NULL, NULL);
+    RSA* rsa_pub2 = PEM_read_RSA_PUBKEY(pub2_fp, NULL, NULL, NULL);
 
     // Extract n and e(integer) from BIGNUM structure(openssl)
-    const BIGNUM *n2_bn = BN_new();
-    const BIGNUM *e2_bn = BN_new();
-    const BIGNUM *d2_bn = BN_new();
-    char *n2_str = NULL;
-    char *e2_str = NULL;
+    const BIGNUM* n2_bn = BN_new();
+    const BIGNUM* e2_bn = BN_new();
+    const BIGNUM* d2_bn = BN_new();
+    char* n2_str = NULL;
+    char* e2_str = NULL;
 
     RSA_get0_key(rsa_pub2, &n2_bn, &e2_bn, &d2_bn);
 
@@ -97,7 +97,7 @@ int main(int argc, char *argv[])
     };
     SHA256(hash_input, total_size, hash_value);
 
-    // converting process(to little endian)
+    // converting endian process
     unsigned char temp = 0x00;
     for (int i = 0; i < 32; i += 4)
     {
@@ -118,13 +118,13 @@ int main(int argc, char *argv[])
     };
 
     // Load Chip Vendor's Private Key
-    FILE *pri1_fp = fopen("argv[2]", "r");
-    if(pri1_fp == NULL)
+    FILE* pri1_fp = fopen(argv[2], "r");
+    if (pri1_fp == NULL)
     {
         printf("\n[ERR] failed to open Chip Vendor's Private Key File\n");
         return -1;
     }
-    RSA *rsa_pri1 = PEM_read_RSAPrivateKey(pri1_fp, NULL, NULL, NULL);
+    RSA* rsa_pri1 = PEM_read_RSAPrivateKey(pri1_fp, NULL, NULL, NULL);
 
     // Private Key Validity Check
     if (RSA_check_key(rsa_pri1) != 1)
@@ -142,13 +142,30 @@ int main(int argc, char *argv[])
         hash_value_padded[k + 4] = hash_value[k];
     }
 
+    for (int i = 0; i < 256; i++)
+    {
+        printf("%02X ", hash_value_padded[i]);
+    }
+
     // RSA_private_encrypt (openssl)
     // @ input : hash_value_padded
     // @ output : cipher_text
     RSA_private_encrypt(RSA_size(rsa_pri1), hash_value_padded, cipher_text, rsa_pri1, RSA_NO_PADDING);
 
+    // converting endian process
+    unsigned char temp_2 = 0x00;
+    for (int i = 0; i < 256; i += 4)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            temp_2 = cipher_text[i + j];
+            cipher_text[i + j] = cipher_text[i + (3 - j)];
+            cipher_text[i + (3 - j)] = temp_2;
+        }
+    }
+
     // Write signature to file
-    FILE *sig_fp = fopen("./output/signature.bin", "wb");
+    FILE* sig_fp = fopen("signature.bin", "wb");
     if (sig_fp == NULL)
     {
         printf("[ERR] failed to open Signature File\n");
@@ -169,7 +186,7 @@ int main(int argc, char *argv[])
     //     printf("%02X ", plain_text[i]);
     // }
 
-    FILE *final_fp = fopen("./output/final_image.bin", "wb");
+    FILE* final_fp = fopen("final_image.bin", "wb");
     if (final_fp == NULL)
     {
         printf("\n[ERR] faile to make final_image.bin\n");
@@ -177,7 +194,7 @@ int main(int argc, char *argv[])
     }
 
     // string type argv[4] (char *) is version number
-    char ver_num[4] = {0x01, 0x00, 0x01, 0x00};
+    char ver_num[4] = { 0x01, 0x00, 0x01, 0x00 };
     fwrite(cipher_text, 1, 256, final_fp);
     fwrite(&bl2_size, 1, 4, final_fp);
     fwrite(ver_num, 1, 4, final_fp);
